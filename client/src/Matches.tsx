d5d1ce14e943b755faa99d2746b48ea46c4e10ef
michal <michalib646@gmail.com>
2021-11-21 11:06:04 +0200

    personal touch

--- client/src/Matches.tsx
+++ client/src/Matches.tsx
@@ -1,5 +1,7 @@
+
 import React, { useState } from "react";
 import { Match } from "./api";
+
 export const Matches = ({
   matches,
   search,
@@ -16,20 +18,17 @@ export const Matches = ({
       t.borrower.user.email.toLowerCase() +
       t.companyName.toLowerCase()
 
-    ).includes(search),
+    ).includes(search.toLowerCase())
 
   );
+  /* defalt sort by credit score */
+  filteredMatches.sort((a, b) => b.borrower.creditScore - a.borrower.creditScore);
+
+
+
+
   const [approve, setApprove] = useState(0);
   const [decline, setDecline] = useState(0);
-  const onApprove = () => {
-    setApprove(approve + 1)
-
-  }
-  const onDecline = () => {
-    setDecline(decline + 1)
-  }
-
-
 
   return (
     <ul className="matches">
@@ -57,13 +56,24 @@ export const Matches = ({
                 {match.borrower.creditScore}
               </p>
             </div>
+
             <div className="score_buttons">
               {/* I add turnery if for credit score */}
               <p>{match.borrower.creditScore < 679 && match.borrower.creditScore > 579 ? <span id="score_B"> B</span> : (match.borrower.creditScore > 679 ? <span id="score_A"> A</span> : <span id="score_C"> C</span>)}</p>
               <div>
-                <button id="Approve" onClick={onApprove} disabled={false}>Approve {approve}</button>
 
-                <button id="Decline" onClick={onDecline}>Decline {decline}</button>
+                <button id="Approve" onClick={() => {
+
+                  setApprove(approve + 1);
+                }} >Approve {approve}</button>
+
+
+
+
+                <button id="Decline" onClick={() => {
+                  setDecline(decline + 1);
+
+                }}>Decline {decline}</button>
               </div>
             </div>
           </div>
